aec69ce5384d620b89f4687fe0e9a98f01e59b2c
Chapiondre <chappen@yandex.ru>
2022-10-12 18:54:37 +0300

    добавила штук из Операторы сравнения

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
-console.log('good mornin fuckin moron')
+// вывод в консоль всякое
+
+let goodMorn = 'good mornin';
+let who = 'fuckin moron';
+
+console.log (`${goodMorn} ${who}`);
+
+//Операторы сравнения
+let x = 3;
+let y = 4;
+
+console.log(x<y);
+
+let string = 'boom';
+
+console.log(x<y);
+
+let string2 = 'kaboom'
+
+console.log(string>string2);
