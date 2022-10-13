2bfe59f1df3347133766f0d74fc906ee3e09de3d
Chapiondre <chappen@yandex.ru>
2022-10-13 22:55:11 +0300

    Логические операторы

--- test.js
+++ test.js
@@ -21,9 +21,9 @@ console.log(string>string2);
 
 // Название JavaScript
 
-let name = prompt('Какое «официальное» название JavaScript?', '');
+let oldName = prompt('Какое «официальное» название JavaScript?', '');
 
-if (name = 'ECMAScript') {
+if (oldName == 'ECMAScript') {
     alert ('Верно!')
 } else {
     alert('Не знаете? “ECMAScript”!')
@@ -48,3 +48,37 @@ let result = (a + b < 4) ? 'Мало' : 'Много';
 // Перепишите 'if..else' в '?'
 
 let message = (login = 'Сотрудник') ? 'Привет' : (login = 'Директор') ? 'Здравствуйте' : (login = '') ? 'Нет логина' : '';
+
+
+
+//Проверка значения из диапазона || Проверка значения вне диапазона
+
+let age = prompt('Скока те лет?', 0);
+
+if (age >= 14 && age <= 90) {
+    alert('Родился, потерпел и умер')
+} else if (age < 14 || age > 90) {
+    alert('Те ваще нельзя пользоваться интернетом')
+} else {
+    alert('Ты код мне не ломай бога ради')
+};
+
+
+//Проверка логина
+
+let login = prompt('Ты кто', '');
+
+if (login == 'Админ') {
+    let password = prompt('а теперь пинкод мы точно не мошенники')
+    if (password == 'Я Главный') {
+        alert('Здравствуйте!')
+    } else if (password === null || password == '') {
+        alert('Отменено')
+    } else {
+        alert('Неверный пароль')
+    }
+} else if (login === null || login == '') {
+    alert('Отменено')
+} else {
+    alert('Ты кто такой я вас не звал')
+}
