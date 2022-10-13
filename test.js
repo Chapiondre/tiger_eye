21f15436cff27f61187fc62ba00673c84a44dff5
Chapiondre <chappen@yandex.ru>
2022-10-13 21:50:35 +0300

    Ветвление и логические операторы. Задачи из learn.javascript

--- test.js
+++ test.js
@@ -18,3 +18,33 @@ console.log(x<y);
 let string2 = 'kaboom'
 
 console.log(string>string2);
+
+// Название JavaScript
+
+let name = prompt('Какое «официальное» название JavaScript?', '');
+
+if (name = 'ECMAScript') {
+    alert ('Верно!')
+} else {
+    alert('Не знаете? “ECMAScript”!')
+};
+
+// Покажите знак числа
+
+let number = promt('Умоляю введи какое-нибудь число не букву', 0);
+
+if (number < 0) {
+    alert(-1)
+} else if (number < 1) {
+    alert(0)
+} else {
+    alert (1)
+};
+
+// Перепишите 'if' в '?'
+
+let result = (a + b < 4) ? 'Мало' : 'Много';
+
+// Перепишите 'if..else' в '?'
+
+let message = (login = 'Сотрудник') ? 'Привет' : (login = 'Директор') ? 'Здравствуйте' : (login = '') ? 'Нет логина' : '';
